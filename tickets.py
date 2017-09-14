d9f0169cb722fd33757e59a44cebf80d38bcff50
kinglion580 <1609019405@qq.com>
2017-09-15 01:27:51 +0800

    update train ticket

--- tickets.py
+++ tickets.py
@@ -17,11 +17,21 @@ Example:
 	tickets -dg chengdu nanjing 2016-10-10
 """
 from docopt import docopt
+from stations import stations
+import requests
+
 def cli():
 	"""command-line interface"""
 	arguments=docopt(__doc__)
-	print(arguments)
+	from_station = stations.get(arguments['<from>'])
+	to_station = stations.get(arguments['<to>'])
+	date = arguments['<date>']
 
+	url='https://kyfw.12306.cn/otn/leftTicket/queryX?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,from_station,to_station)
+	#print(url)
+	r=requests.get(url,verify=False)
+	print(r.text)
+	#print(r.json())
 if __name__=='__main__':
 	cli()
 
