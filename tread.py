e90db18d56cbac8e035d5a9e476167ee8f6be0ce
kinglion580 <1609019405@qq.com>
2017-09-18 17:40:35 +0800

    test thread

---
+++ tread.py
@@ -0,0 +1,17 @@
+import os
+
+from multiprocessing import Process
+
+def hello(name):
+	print('child process:{}'.format(os.getpid()))
+	print('hello'+name)
+
+def main():
+	p=Process(target=hello,args=('shiyanlou',))
+	p.start()
+	p.join()
+	print('parent process: {}'.format(os.getpid()))
+
+if __name__=='__main__':
+	main()
+
