4ab153234630335cfd55f0c392d0300517122a5f
kinglion580 <1609019405@qq.com>
2017-09-18 16:52:11 +0800

    test Decorator

---
+++ testDecorator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+def log(func):
+	def decorator(*args,**kwargs):
+		print('Function '+func.__name__+' has been called at '+datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		return func(*args,**kwargs)
+	return decorator
+
+
+@log
+def add(x,y):
+	return x+y
+
+@log 
+def cut(x,y):
+	return x-y
+
+@log
+def show():
+	return "hello shiyanlou"
+
+
+if __name__=="__main__":
+	add(1,2)
+	cut(5,3)
+	show()
