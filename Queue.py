da960fc9070b2cfb09e9d99ac5c46041c6ae34fd
kinglion580 <1609019405@qq.com>
2017-09-18 18:21:03 +0800

    test Queue

---
+++ Queue.py
@@ -0,0 +1,23 @@
+from multiprocessing import Queue,Process
+
+queue=Queue()
+
+def f1():
+	queue.put('Hello shiyanlou')
+	#conn1.send('Hello shiyanlou')
+	queue.put('Hello shiyanlou2')
+	queue.put('Hello shiyanlou3')
+
+def f2():
+	data=queue.get()
+	data=queue.get()
+	#data=conn2.recv()
+	#data=conn2.recv()
+	print(data)
+
+def main():
+	Process(target=f1).start()
+	Process(target=f2).start()
+
+if __name__=='__main__':
+	main()
