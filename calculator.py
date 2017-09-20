f9bf26f584925b4e6e27787293a52a870c2e06d9
kinglion <kinglion580@outlook.com>
2017-09-20 14:48:48 +0800

    优化-》命名元组

--- calculator.py
+++ calculator.py
@@ -1,33 +1,44 @@
+from collections import namedtuple
+
+IncomeTaxQuickLookupItem = namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
+
+INCOME_TAX_START_POINT = 3500
+
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 13505),
+    IncomeTaxQuickLookupItem(35000, 0.30, 2755),
+    IncomeTaxQuickLookupItem(55000, 0.35, 5505),
+    IncomeTaxQuickLookupItem(9000, 0.25, 1005),
+    IncomeTaxQuickLookupItem(4500, 0.2, 555),
+    IncomeTaxQuickLookupItem(1500, 0.1, 105),
+    IncomeTaxQuickLookupItem(0, 0.03, 0)
+]
+
+
 def calc_income_tax(income):
-    taxable_part = income - 3500
+    taxable_part = income - INCOME_TAX_START_POINT
     if taxable_part <= 0:
         return '0.00'
-    income_tax_quick_lookup_table = [
-        (80000, 0.45, 13505),
-        (35000, 0.30, 2755),
-        (55000, 0.35, 5505),
-        (9000, 0.25, 1005),
-        (4500, 0.2, 555),
-        (1500, 0.1, 105),
-        (0, 0.03, 0)
-    ]
-    for item in income_tax_quick_lookup_table:
-        if taxable_part > item[0]:
-            result = taxable_part * item[1] - item[2]
-            return print('{:.2f}'.format(result))
+
+    for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+        if taxable_part > item.start_point:
+            tax = taxable_part * item.tax_rate - item.quick_subtractor
+            return '{:.2f}'.format(tax)
 
 
 def main():
     import sys
     if len(sys.argv) != 2:
         print("Parameter Error")
-        exit()
     try:
         income = int(sys.argv[1])
     except ValueError:
         print("Parameter Error")
-        exit()
     print(calc_income_tax(income))
+
 
 if __name__ == '__main__':
     main()
