4897622db43917eecf56d1e733a08f775b20c1ce
kinglion580 <1609019405@qq.com>
2017-10-09 17:11:18 +0800

    重构优化

--- flask_test/news/app.py
+++ flask_test/news/app.py
@@ -1,33 +1,51 @@
 import os
 import json
-from flask import Flask,render_template,abort
+from flask import Flask, render_template, abort
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+
+class Files(object):
+    directory = os.path.join(os.path.abspath(os.path.dirname(__name__)), '..', 'files')
+
+    def __init__(self):
+        self._file = self._read_all_files()
+
+    def _read_all_files(self):
+        result = {}
+        for filename in os.listdir(self.directory):
+            file_path = os.path.join(self.directory, filename)
+            with open(file_path) as f:
+                result[filename[:-5]] = json.load(f)
+        return result
+
+    def get_title_list(self):
+        return [item['title'] for item in self._file.values()]
+
+    def get_by_filename(self, filename):
+        return self._file.get(filename)
+
+
+files = Files()
+
 
 @app.route('/')
 def index():
-    str=[]
-    for i in os.listdir('../files'):
-        with open('../files/'+i) as f:
-          str.append(json.load(f)['title'])  
-    return render_template('index.html',title_list=str)
+    return render_template('index.html', title_list=files.get_title_list())
 
 
 @app.route('/files/<filename>')
 def file(filename):
-    path='../files/'+filename+'.json'
-    if not os.path.exists(path):
+    item = files.get_by_filename(filename)
+    if not item:
         abort(404)
-    else:
-        with open(path) as f:
-            item=json.load(f)
-        return render_template('file.html',item_list=item)
+    return render_template('file.html', item_list=item)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
